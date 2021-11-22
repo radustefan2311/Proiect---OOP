b83182e5ebe9d506dce4cd7fb84a5648185392e9
Radu <radustefan2311@gmail.com>
2021-11-22 12:59:34 +0200

    Realizat transmitere parametrii prin referinta + Sters clasa Sala + Adaugare clasa login

---
+++ headers/Login.h
@@ -0,0 +1,29 @@
+//
+// Created by Radu on 22.11.2021.
+//
+
+#include"Client.h"
+#include"Rezervare.h"
+#include <iostream>
+#include <vector>
+
+#ifndef PROIECT___OOP_LOGIN_H
+#define PROIECT___OOP_LOGIN_H
+
+class Login {
+
+    std::vector<Client> logare;
+public:
+    void functie_login(const Client &client);
+
+};
+
+
+
+
+
+
+
+
+
+#endif //PROIECT___OOP_LOGIN_H
---
+++ src/Login.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Radu on 22.11.2021.
+//
+
+#include "../headers/Login.h"
+
+void Login::functie_login(const Client &client) {
+
+
+
+
+
+}
